2426bbe304dd44aa35249ee0bc6b4404f55bc1d6
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-04-17 20:08:31 +0200

    Refactor calculator script to use consistent variable names

--- scripts/calculadora.sh
+++ scripts/calculadora.sh
@@ -4,35 +4,35 @@ echo "Deseas SUMAR, RESTAR, MULTIPLICAR o DIVIDIR?"
 read op
 if [ "$op" = SUMAR ]; then
 	echo "Has elegido sumar, escriba el primer número."
-	read suma1
-	echo "Tu primer número es $suma1, escriba el siguiente numero."
-	read suma2
-	suma=$((suma1 + suma2))
-	echo "El resultado de tu suma es $suma."
+	read num1
+	echo "Tu primer número es $num, escriba el siguiente numero."
+	read num2
+	resultado=$((num1 + num2))
+	echo "El resultado de tu suma es $resultado."
 
 elif [ "$op" = RESTAR ]; then
         echo "Has elegido restar, escriba el primer número."
-        read resta1
-        echo "Tu primer número es $resta1, escriba el siguiente numero."
-        read resta2
-        resta=$((resta1 - resta2))
-        echo "El resultado de tu resta es $resta."
+        read num1
+        echo "Tu primer número es $num1, escriba el siguiente numero."
+        read num2
+        resultado=$((num1 - num2))
+        echo "El resultado de tu resta es $resultado."
 
 elif [ "$op" = MULTIPLICAR ]; then
         echo "Has elegido multiplicar, escriba el primer número."
-        read mult1
-        echo "Tu primer número es $mult1, escriba el siguiente numero."
-        read mult2
-        mult=$((mult1 * mult2))
-        echo "El resultado de tu multiplicación es $mult."
+        read num1
+        echo "Tu primer número es $num1, escriba el siguiente numero."
+        read num2
+        resultado=$((num1 * num2))
+        echo "El resultado de tu multiplicación es $resultado."
 
 elif [ "$op" = DIVIDIR ]; then
         echo "Has elegido dividir, escriba el primer número."
-        read divd1
-        echo "Tu primer número es $divd1, escriba el siguiente numero."
-        read divd2
-        divd=$((divd1 / divd2))
-        echo "El resultado de tu división es $divd."
+        read num1
+        echo "Tu primer número es $num1, escriba el siguiente numero."
+        read num2
+        resultado=$((num1 / num2))
+        echo "El resultado de tu división es $resultado."
 
 else
 	echo "Opción no valida. Solo puedes elegir: SUMAR, RESTAR, MULTIPLICAR o DIVIDIR."
