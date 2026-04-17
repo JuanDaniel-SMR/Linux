0d99e1a5214f5781d0420490722c18f670d85f30
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-04-17 14:36:58 +0200

    Add script to calculate triangle area

---
+++ scripts/triangle_area.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Bienvenido a calcular el área de un triangulo."
+echo "Indique la altura de su triangulo."
+read height
+echo "Indique la base de su traingulo."
+read base
+triangle_area=$((height * base /2))
+echo "El área de su triangulo es $triangle_area"
